d9950e4461227a625e2202028076f7267cf73c39
Ganesh Narayana Raju Bhupathi Raju <gnrbhupathiraju@adaequare.com>
2019-12-26 15:45:11 +0530

    Terraform Module with ALB changes in json files

--- installation-script.sh
+++ installation-script.sh
@@ -1,8 +1,16 @@
-#! /bin/bash
+sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install -y apache2
-sudo apt-get install -y php libapache2-mod-php php-mysql php-curl php-gd php-json php-zip php-mbstring
+sudo apt-get install -y php7.2
+sudo apt-get install -y php7.2-mysql
+sudo apt-get install -y php7.2-cli php7.2-common libapache2-mod-php7.2 php7.2 php7.2-curl
+sudo apt-get install -y php7.2-mbstring
 sudo a2enmod rewrite
 sudo a2enmod ssl
+
 sudo systemctl start apache2
 sudo systemctl enable apache2
+
+hostname | sudo tee /var/www/html/index.html
+sudo touch /var/www/html/phpinfo.php
+echo "\"<?php phpinfo(); ?>"\" | sudo tee /var/www/html/phpinfo.php
